9138f490b181915020e6aa8a9a19ff6ce07d7f02
Ilya Filimonov <ilyafil1996@yandex.ru>
2021-05-14 21:42:25 +0300

    FailTest_Homework_v.1

--- src/FailJCBTest.java
+++ src/FailJCBTest.java
@@ -1,7 +1,7 @@
-public class Main {
+public class FailJCBTest {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "5351719427810741";
+        String number = "3537285923580102181";
         System.out.printf("Result is %s%n", isValidCardNumber(number) ? "OK" : "FAIL");
     }
 
